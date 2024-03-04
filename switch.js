40d9e4fb87c7b883374fb251e87c91e072d778aa
Carleoinserra <inserracarlo@gmail.com>
2024-03-04 14:27:35 +0100

    Create switch.js

---
+++ switch.js
@@ -0,0 +1,27 @@
+var num = parseInt(prompt("Scrivi un numero: "));
+
+switch (num) {
+case 1:
+    console.log("Giorno normale");
+    break;
+case 2:
+    console.log("Giorno normale");
+    break;
+case 3:
+    console.log("Giorno normale");
+    break;
+case 4:
+    console.log("Sta arrivando il weekend");
+    break;
+case 5:
+    console.log("Ancora un altro pò");
+    break;
+case 6:
+    console.log("Finalmente sabato");
+    break;
+    case 7:
+    console.log("Relax");
+    break;
+    default:
+    console.log("Non è un giorno della setimana");
+}
